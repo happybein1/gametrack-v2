f1c3c526b3ef244899654dd1d8775aa56ddf0b3e
happybein <happybein1@gmail.com>
2026-05-11 23:31:18 +0200

    first commit

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'voice-umpire-v2-7-20260511-7';
+const CACHE_NAME = 'voice-umpire-v2-8-20260511-8';
 const STATIC_ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (e) => {
